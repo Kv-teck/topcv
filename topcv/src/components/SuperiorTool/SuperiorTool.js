9b8b32198328888fde5fc722847d771503fa0f38
Kv-teck <vukhiet2k3@gmail.com>
2024-09-17 23:44:55 +0700

    Chỉnh sửa

--- topcv/src/components/SuperiorTool/SuperiorTool.js
+++ topcv/src/components/SuperiorTool/SuperiorTool.js
@@ -1,45 +1,46 @@
+
 export const SuperiorTool = () => {
     return (
         <>
             <div className="container mx-auto pt-12 ">
-                <h1 className="text-2xl font-bold text-green-600 mb-6 ">Công cụ vượt trội!</h1>
-                <div className=" pb-14 items-center bg-center bg-no-repeat bg-[length:340px_387px] grid gap-[5%_35%] grid-cols-2 justify-between mt-5 pt-14  bg-[url('https://static.topcv.vn/v4/image/welcome/superior-tool/superior-tool-bg.png?v=1.0.1')] ">
+                <h1 className="text-4xl	 font-bold text-green-600 mb-6">Công cụ vượt trội!</h1>
+                <div className=" pb-20 items-center bg-center bg-no-repeat bg-[length:340px_387px] grid gap-[5%_35%] grid-cols-2 justify-between mt-5 pt-14  bg-[url('https://static.topcv.vn/v4/image/welcome/superior-tool/superior-tool-bg.png?v=1.0.1')] ">
 
-                    <div class="content__session grid grid-cols-1 gap-6 ">
-                        <a href="https://www.topcv.vn/tinh-luong-gross-net" class="item bg-white p-4 rounded-lg shadow-md hover:shadow-lg flex items-center space-x-4 w-fit ">
-                            <div class="icon">
-                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/gross-net.png" alt="Tính lương GROSS - NET" class="w-12 h-12" />
+                    <div className="content__session grid grid-cols-1 gap-6 ">
+                        <a href="https://www.topcv.vn/tinh-luong-gross-net" className="item bg-white p-4 rounded-lg shadow-md flex items-center space-x-4 w-fit mr-auto hover:shadow hover:shadow-green-700 ">
+                            <div className="icon">
+                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/gross-net.png" alt="Tính lương GROSS - NET" className="w-12 h-12" />
                             </div>
-                            <div class="title">
-                                <h3 class="font-semibold text-lg text-gray-800">Tính lương GROSS - NET</h3>
-                                <span class="text-green-500 flex items-center space-x-1">Khám phá ngay
-                                    <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <div className="title">
+                                <h3 className="font-semibold text-xl text-gray-800">Tính lương GROSS - NET</h3>
+                                <span className="text-green-500 flex items-center space-x-1">Khám phá ngay
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
                                     </svg>
                                 </span>
                             </div>
                         </a>
-                        <a href="https://www.topcv.vn/tinh-lai-kep" class="item bg-white p-4 rounded-lg shadow-md hover:shadow-lg flex items-center space-x-4 w-fit">
-                            <div class="icon">
-                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/lai-suat-kep.png" alt="Tính lãi suất kép" class="w-12 h-12" />
+                        <a href="https://www.topcv.vn/tinh-lai-kep" className="item bg-white p-4 rounded-lg shadow-md flex items-center space-x-4 w-fit mr-auto hover:shadow hover:shadow-green-700">
+                            <div className="icon">
+                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/lai-suat-kep.png" alt="Tính lãi suất kép" className="w-12 h-12" />
                             </div>
-                            <div class="title">
-                                <h3 class="font-semibold text-lg text-gray-800">Tính lãi suất kép</h3>
-                                <span class="text-green-500 flex items-center space-x-1">Khám phá ngay
-                                    <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <div className="title">
+                                <h3 className="font-semibold text-xl text-gray-800">Tính lãi suất kép</h3>
+                                <span className="text-green-500 flex items-center space-x-1">Khám phá ngay
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
                                     </svg>
                                 </span>
                             </div>
                         </a>
-                        <a href="https://www.topcv.vn/tinh-thue-thu-nhap-ca-nhan" class="item bg-white p-4 rounded-lg shadow-md hover:shadow-lg flex items-center space-x-4 w-fit">
-                            <div class="icon">
-                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/thu-nhap-ca-nhan.png" alt="Tính thuế thu nhập cá nhân" class="w-12 h-12" />
+                        <a href="https://www.topcv.vn/tinh-thue-thu-nhap-ca-nhan" className="item bg-white p-4 rounded-lg shadow-md flex items-center space-x-4 w-fit mr-auto hover:shadow hover:shadow-green-700">
+                            <div className="icon">
+                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/thu-nhap-ca-nhan.png" alt="Tính thuế thu nhập cá nhân" className="w-12 h-12" />
                             </div>
-                            <div class="title">
-                                <h3 class="font-semibold text-lg text-gray-800">Tính thuế thu nhập cá nhân</h3>
-                                <span class="text-green-500 flex items-center space-x-1">Khám phá ngay
-                                    <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <div className="title">
+                                <h3 className="font-semibold text-xl text-gray-800">Tính thuế thu nhập cá nhân</h3>
+                                <span className="text-green-500 flex items-center space-x-1">Khám phá ngay
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
                                     </svg>
                                 </span>
@@ -48,42 +49,41 @@ export const SuperiorTool = () => {
                     </div>
 
 
-
-                    <div class="content__session grid grid-cols-1 gap-6">
-                        <a href="https://www.topcv.vn/cong-cu-tinh-muc-huong-bao-hiem-that-nghiep" class="item bg-white p-4 rounded-lg shadow-md hover:shadow-lg flex items-center space-x-4 w-fit">
-                            <div class="icon">
-                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/bao-hiem-that-nghiep.png" alt="Tính Bảo hiểm thất nghiệp" class="w-12 h-12" />
+                    <div className="content__session grid grid-cols-1 gap-6">
+                        <a href="https://www.topcv.vn/cong-cu-tinh-muc-huong-bao-hiem-that-nghiep" className="item bg-white p-4 rounded-lg shadow-md flex items-center space-x-4 w-fit ml-auto hover:shadow hover:shadow-green-700">
+                            <div className="icon">
+                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/bao-hiem-that-nghiep.png" alt="Tính Bảo hiểm thất nghiệp" className="w-12 h-12" />
                             </div>
-                            <div class="title">
-                                <h3 class="font-semibold text-lg text-gray-800">Tính Bảo hiểm thất nghiệp</h3>
-                                <span class="text-green-500 flex items-center space-x-1">Khám phá ngay
-                                    <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <div className="title">
+                                <h3 className="font-semibold text-xl text-gray-800">Tính Bảo hiểm thất nghiệp</h3>
+                                <span className="text-green-500 flex items-center space-x-1">Khám phá ngay
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
                                     </svg>
                                 </span>
                             </div>
                         </a>
-                        <a href="https://www.topcv.vn/lap-ke-hoach-tiet-kiem" class="item bg-white p-4 rounded-lg shadow-md hover:shadow-lg flex items-center space-x-4 w-fit">
-                            <div class="icon">
-                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/ke-hoach-tiet-kiem.png" alt="Lập kế hoạch tiết kiệm" class="w-12 h-12" />
+                        <a href="https://www.topcv.vn/lap-ke-hoach-tiet-kiem" className="item bg-white p-4 rounded-lg shadow-md flex items-center space-x-4 w-fit ml-auto hover:shadow hover:shadow-green-700">
+                            <div className="icon">
+                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/ke-hoach-tiet-kiem.png" alt="Lập kế hoạch tiết kiệm" className="w-12 h-12" />
                             </div>
-                            <div class="title">
-                                <h3 class="font-semibold text-lg text-gray-800">Lập kế hoạch tiết kiệm</h3>
-                                <span class="text-green-500 flex items-center space-x-1">Khám phá ngay
-                                    <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <div className="title">
+                                <h3 className="font-semibold text-xl text-gray-800">Lập kế hoạch tiết kiệm</h3>
+                                <span className="text-green-500 flex items-center space-x-1">Khám phá ngay
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
                                     </svg>
                                 </span>
                             </div>
                         </a>
-                        <a href="https://www.topcv.vn/tinh-bao-hiem-xa-hoi-mot-lan" class="item bg-white p-4 rounded-lg shadow-md hover:shadow-lg flex items-center space-x-4 w-fit">
-                            <div class="icon">
-                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/bao-hiem-xa-hoi-mot-lan.png" alt="Tính bảo hiểm xã hội một lần" class="w-12 h-12" />
+                        <a href="https://www.topcv.vn/tinh-bao-hiem-xa-hoi-mot-lan" className="item bg-white p-4 rounded-lg shadow-md flex items-center space-x-4 w-fit ml-auto hover:shadow hover:shadow-green-700">
+                            <div className="icon">
+                                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/superior-tool/bao-hiem-xa-hoi-mot-lan.png" alt="Tính bảo hiểm xã hội một lần" className="w-12 h-12" />
                             </div>
-                            <div class="title">
-                                <h3 class="font-semibold text-lg text-gray-800">Tính bảo hiểm xã hội một lần</h3>
-                                <span class="text-green-500 flex items-center space-x-1">Khám phá ngay
-                                    <svg class="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <div className="title">
+                                <h3 className="font-semibold text-xl text-gray-800">Tính bảo hiểm xã hội một lần</h3>
+                                <span className="text-green-500 flex items-center space-x-1">Khám phá ngay
+                                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"></path>
                                     </svg>
                                 </span>
