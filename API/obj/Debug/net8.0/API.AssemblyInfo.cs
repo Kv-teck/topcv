0f61851db5d24c571d0696083dd80af810682a59
DoanDinh0324 <dinhdoan0324@gmail.com>
2024-09-20 20:54:36 +0700

    router-dom

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4354130dc40027c0c08167151e72acf1ded8759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4251eb4ef457d5ab101bf528f4429d900979ba81")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
