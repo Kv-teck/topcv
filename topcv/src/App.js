8cc0a1eb1500581e43c8363b7f00856093b528cf
Kv-teck <vukhiet2k3@gmail.com>
2024-09-20 21:01:40 +0700

    DEMO

--- topcv/src/App.js
+++ topcv/src/App.js
@@ -1,31 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import FooterKeyword from './components/Footer/FooterKeyword';
 import FooterMain from './components/Footer/FooterMain';
 import HeaderComponent from './components/Headercomponent/HeaderComponent';
 import { HomePage } from './pages/HomePage/HomePage';
-import MarketInfo from './Redux/MarketInfo/MarketInfo';
-import SearchSection from './Redux/Slider/SearchSection';
-import Videobaner from './Redux/Video/Videobaner';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
-      <div className="bg-[url('https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/bg_header.webp')] bg-[length:100%]">
-        <div className=" container mx-auto">
-          <div className='flex'>
-            <div>
-              <SearchSection />
-              <div className="container mx-auto p-8">
-                <Videobaner />
-              </div>
-            </div>
-            <MarketInfo />
-          </div>
-        </div>
-      </div>
+
       <main>
-        <HomePage />
+        <Routes>
+          <Route path='/' Component={HomePage} />
+        </Routes>
       </main>
 
       <footer>
--- topcv/src/pages/HomePage/HomePage.jsx
+++ topcv/src/pages/HomePage/HomePage.jsx
@@ -5,12 +5,28 @@ import TopProminent from '../../components/Top/TopProminent/TopProminent'
 import SuperiorTool from '../../components/SuperiorTool/SuperiorTool'
 import AchievementAward from '../../components/TopCV/AchievementAward'
 import PressAboutTopCV from '../../components/TopCV/PressAboutTopCV'
+import SearchSection from '../../Redux/Slider/SearchSection'
+import Videobaner from '../../Redux/Video/Videobaner'
+import MarketInfo from '../../Redux/MarketInfo/MarketInfo'
 
 
 export const HomePage = () => {
   return (
     <>
-      <main>
+      <div className="bg-[url('https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/bg_header.webp')] bg-[length:100%]">
+        <div className=" container mx-auto">
+          <div className='flex'>
+            <div>
+              <SearchSection />
+              <div className="container mx-auto p-8">
+                <Videobaner />
+              </div>
+            </div>
+            <MarketInfo />
+          </div>
+        </div>
+      </div>
+
         <div className='bg-gray-100'>
           <ProductTop />
         </div>
@@ -23,7 +39,7 @@ export const HomePage = () => {
           <AchievementAward />
         </div >
         <PressAboutTopCV />
-      </main>
+
 
     </>
   )
