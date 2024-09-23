ff861a003ccebbbeed3049f0ab01a140986891f9
Kv-teck <vukhiet2k3@gmail.com>
2024-09-23 20:08:32 +0700

    DEMO

--- topcv/src/components/Footer/BackTop.js
+++ topcv/src/components/Footer/BackTop.js
@@ -1,8 +1,8 @@
 const BackTop = () => {
     return (
-        <a href="#" className="bg-green-500 text-white text-xl rounded-full fixed bottom-[3.25rem] right-[4.25rem] w-16 h-16 flex items-center justify-center shadow-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-up" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V3.707l3.147 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 .708.708L7.5 3.707V11.5a.5.5 0 0 0 .5.5z" />
+        <a href="#" className="bg-gradient-to-r from-green-500 to-green-700 text-white rounded-full fixed bottom-[3.25rem] right-[4.25rem] w-14 h-14 flex items-center justify-center shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-up" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V3.707l3.147 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 .708.708L7.5 3.707V11.5a.5.5 0 0 0 .5.5z" />
             </svg>
         </a>
 
