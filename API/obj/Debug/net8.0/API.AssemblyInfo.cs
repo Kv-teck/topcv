9d9a4a799192408daf3acf6297b939be523dfe8b
DoanDinh0324 <dinhdoan0324@gmail.com>
2024-09-23 18:58:43 +0700

    API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6145dac5fb02b626acaa4dd21a31ec3e83e863b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b4bc645dcd8ecf4e9c564b2f9bf27dc25721d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
