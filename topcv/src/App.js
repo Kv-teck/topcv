888872ba393ba9abe978fd43e4f8e8cb1be4d1a3
Kv-teck <vukhiet2k3@gmail.com>
2024-09-23 19:30:07 +0700

    demo

--- topcv/src/App.js
+++ topcv/src/App.js
@@ -2,9 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import FooterKeyword from './components/Footer/FooterKeyword';
 import FooterMain from './components/Footer/FooterMain';
+import BackTop from './components/Footer/BackTop';
 import HeaderComponent from './components/Headercomponent/HeaderComponent';
 import { HomePage } from './pages/HomePage/HomePage';
 import Login from './services/Login';
+import Register from './services/Register';
 
 
 function App() {
@@ -15,14 +17,15 @@ function App() {
       <main>
         <Routes>
           <Route path='/' Component={HomePage} />
-          <Route path='/login' Component={Login} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </main>
 
       <footer>
         <FooterKeyword />
         <FooterMain />
-        <BackTop/>
+        <BackTop />
       </footer>
     </div>
 
