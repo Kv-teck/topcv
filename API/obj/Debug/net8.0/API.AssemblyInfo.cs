bcbe3e79bd84dc97b01cda20dcfb6e66e923d31f
DoanDinh0324 <dinhdoan0324@gmail.com>
2024-09-24 15:18:49 +0700

    loginAPI

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9a4a799192408daf3acf6297b939be523dfe8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd309b43fc683f2cea219780229d107ca578290f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
