3f2e1b39fc6173b839f1c1673005a8bf1a6e6edc
DoanDinh0324 <dinhdoan0324@gmail.com>
2024-09-27 23:52:13 +0700

    isLoading API

--- topcv/src/members/Profile.jsx
+++ topcv/src/members/Profile.jsx
@@ -4,13 +4,17 @@ import { getMemberById } from '../environments/apiService'; // Đảm bảo impo
 
 const Profile = () => {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true); // Thêm state cho loading
+    const [error, setError] = useState(null); // Thêm state cho lỗi
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchUserData = async () => {
             const username = localStorage.getItem('username'); // Lấy username từ local storage
             if (!username) {
                 navigate('/login'); // Redirect nếu không có username
+                setIsLoading(false); // Kết thúc loading
                 return;
             }
             try {
@@ -18,14 +22,31 @@ const Profile = () => {
                 setUser(userData);
             } catch (error) {
                 console.error('Lỗi khi lấy thông tin người dùng:', error);
+                setError('Có lỗi xảy ra khi lấy thông tin người dùng. Vui lòng thử lại.'); // Cập nhật thông báo lỗi
                 navigate('/login'); // Redirect nếu có lỗi
+            } finally {
+                setIsLoading(false); // Kết thúc loading
             }
         };
 
         fetchUserData();
     }, [navigate]);
 
-    if (!user) return <div>Loading...</div>; // Trạng thái loading
+    if (isLoading) 
+        return (
+            <div id="spinner" className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-75">
+                <div className="animate-spin h-16 w-16 border-4 border-green-500 border-t-transparent rounded-full" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
+        ); // Trạng thái loading
+
+    if (error) // Kiểm tra lỗi và hiển thị thông báo
+        return (
+            <div className="container mx-auto p-4">
+                <p className="text-red-500">{error}</p>
+            </div>
+        );
 
     return (
         <div className="container mx-auto p-4">
